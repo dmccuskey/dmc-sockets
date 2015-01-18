cc1249ee720ffd84e17d2d92314770b73f38f030
dmc <my-github@mccuskey.com>
2015-01-18 15:48:04 -0700

    clean up – comments, version

--- examples/dmc-sockets-basic/main.lua
+++ examples/dmc-sockets-basic/main.lua
@@ -3,32 +3,37 @@
 --
 -- Tests the Sockets library
 --
--- by David McCuskey
---
 -- Sample code is MIT licensed, the same license which covers Lua itself
 -- http://en.wikipedia.org/wiki/MIT_License
 -- Copyright (C) 2014 David McCuskey. All Rights Reserved.
 --====================================================================--
 
 
-print("===================================================================")
+
+print("\n\n#######################################################\n\n")
+
 
 
 --===================================================================--
--- Imports
+--== Imports
+
 
 local Sockets = require 'dmc_corona.dmc_sockets'
 
 
+
 --====================================================================--
--- Setup, Constants
+--== Setup, Constants
+
 
 local host, port = 'docs.davidmccuskey.com', 80
 local sock
 
 
+
 --====================================================================--
--- Support Functions
+--== Support Functions
+
 
 -- make up a generic request for the web server
 --
@@ -40,7 +45,7 @@ local function sendRequest()
 	local req_t = {
 		"GET / HTTP/1.1\r\n",
 		"Host: " .. host .. "\r\n",
-		"User-Agent: DMC Sockets " .. Sockets.VERSION .. "\r\n",
+		"User-Agent: DMC Sockets " .. Sockets.version .. "\r\n",
 		"\r\n"
 	}
 	req = table.concat( req_t, "" )
@@ -94,9 +99,12 @@ local function socketCallback( event )
 end
 
 
+
 --====================================================================--
--- Main
+--== Main
 --====================================================================--
+
+
 
 sock = Sockets:create( Sockets.TCP )
 sock:addEventListener( sock.EVENT, socketCallback )
